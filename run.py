fce54ab802fcc2e3afe877ad3a47554599e79ac6
NycollasSantos <nycollassantos.ads@gmail.com>
2025-05-28 15:14:05 -0300

    Add Vercel configuration

--- run.py
+++ run.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
+# This is for Vercel deployment
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use production settings when deployed
+    app.config['DEBUG'] = False
+    app.run(host='0.0.0.0', port=8000)
